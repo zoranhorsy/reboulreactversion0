1123d2718621c0df683a5048aaa5b4362c48c12f
zoran <zxransounds@gmail.com>
2025-08-12 14:54:48 +0200

    feat: Ajout des dépendances `@vercel/analytics` et `@vercel/speed-insights`
    - Intégration des bibliothèques pour améliorer l'analyse des performances et le suivi des utilisateurs.
    - Mise à jour des fichiers `package.json` et `package-lock.json` pour inclure les nouvelles dépendances.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import OptimizedHomeContent from "@/components/optimized/OptimizedHomeContent";
-
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Page() {
   return <OptimizedHomeContent />;
 }
