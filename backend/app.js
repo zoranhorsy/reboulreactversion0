83fbf69169485e6daf5e14fc741d6de6ba16a673
zoran <zxransounds@gmail.com>
2025-08-20 12:19:44 +0200

    fix: re-enable collections carousel route

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const addressesRoutes = require('./routes/addresses');
 const cornerProductsRoutes = require('./routes/cornerProducts');
 const cornerProductVariantsRoutes = require('./routes/cornerProductVariants');
 const contactRoutes = require('./routes/contact');
-// const collectionsCarouselRoutes = require('./routes/collectionsCarousel');
+const collectionsCarouselRoutes = require('./routes/collectionsCarousel');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
@@ -76,7 +76,7 @@ app.use('/api/addresses', addressesRoutes);
 app.use('/api/corner-products', cornerProductsRoutes);
 app.use('/api/corner-product-variants', cornerProductVariantsRoutes);
 app.use('/api/contact', contactRoutes);
-// app.use('/api/collections-carousel', collectionsCarouselRoutes);
+app.use('/api/collections-carousel', collectionsCarouselRoutes);
 app.use("/api/sneakers-products", require("./routes/sneakersProducts"));
 app.use("/api/minots-products", require("./routes/minotsProducts"));
 
